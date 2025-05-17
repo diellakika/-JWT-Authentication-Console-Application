30ad778d200d31feec87c15dd618b779e203a612
Erisa Ramadani <erisa.ramadani@student.uni-pr.edu>
2025-05-17 19:36:23 +0200

    Update TockenBlackList.java

--- JWT/src/util/TockenBlackList.java
+++ JWT/src/util/TockenBlackList.java
@@ -1,19 +1,1 @@
-package util;
 
-import java.util.HashSet;
-import java.util.Set;
-
-public class TokenBlacklist {
-    private static final Set<String> blacklistedTokens = new HashSet();
-
-    public TokenBlacklist() {
-    }
-
-    public static void add(String token) {
-        blacklistedTokens.add(token);
-    }
-
-    public static boolean contains(String token) {
-        return blacklistedTokens.contains(token);
-    }
-}
