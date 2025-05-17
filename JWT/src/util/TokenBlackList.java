824ad4035f3d7fc13f93ca53d978e7bfb34274c6
Enis Shabani <shabanienis44@gmail.com>
2025-05-17 19:48:27 +0200

    Added TokenBlacklist

--- JWT/src/util/TokenBlackList.java
+++ JWT/src/util/TokenBlackList.java
@@ -1,1 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TokenBlacklist {
+    private static final Set<String> blacklistedTokens = new HashSet<>();
+
+    public static void add(String token) {
+        blacklistedTokens.add(token);
+    }
+
+    public static boolean contains(String token) {
+        return blacklistedTokens.contains(token);
+    }
+}
 
